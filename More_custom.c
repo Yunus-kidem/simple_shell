58e259f32c57b8ed9245f1ee91c5854602beaf09
Yunus-kidem <74186011+Yunus-kidem@users.noreply.github.com>
2021-08-13 18:25:20 +0300

    Removed the 6th functions per file

--- More_custom.c
+++ More_custom.c
@@ -113,19 +113,3 @@ void array_rev(char *arr, int len)
 		arr[(len - 1) - i] = tmp;
 	}
 }
-/**
- * intlen - Determine Length Of Int
- * @num: Given Int
- * Return: Length Of Int
- */
-int intlen(int num)
-{
-	int len = 0;
-
-	while (num != 0)
-	{
-		len++;
-		num /= 10;
-	}
-	return (len);
-}
