5d0b20feccc6af410779d2ea7d0c2170a89e4075
Yunus-kidem <74186011+Yunus-kidem@users.noreply.github.com>
2021-08-13 18:17:05 +0300

    Added prototypes for the custom functions from printf project

--- holberton.h
+++ holberton.h
@@ -1,14 +1,42 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/**###### environ var ######*/
+
+extern char **environ;
+
+/**###### LIBS USED ######*/
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
-#include <stdarg.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <errno.h>
+#include <linux/limits.h>
+
+/**###### STRING FUNCTION ######*/
+
+char *_strtok(char *str, const char *tok);
+unsigned int check_delim(char c, const char *str);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+int _putchar(char c);
+int _atoi(char *s);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+int _isalpha(int c);
+void array_rev(char *arr, int len);
+int intlen(int num);
+char *_itoa(unsigned int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strchr(char *s, char c);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strdup(char *str);
 
 #endif /* HOLBERTON_H */
