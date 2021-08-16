6da813be460a3cd05ebb769356d0749b42d27c29
obed101 <obedamoako92@gmail.com>
2021-08-16 04:16:44 -0700

    :wrench: Fixed Multifunction Problem

--- custom_printf.c
+++ custom_printf.c
@@ -1,32 +1,6 @@
 #include "holberton.h"
 
 /**
- * _strncpy - copies src string to dest string
- * @dest:char
- *  @src:char
- * @n:int
- * Return:char
- */
-
-char *_strncpy(char *dest, char *src, int n)
-{
-int i;
-
-i = 0;
-	while (i < n && *(src + i))
-	{
-	*(dest + i) = *(src + i);
-	i++;
-	}
-	while (i < n)
-	{
-	*(dest + i) = '\0';
-	i++;
-	}
-	return (dest);
-}
-
-/**
  * _strlen - Counts the length of a string
  * @s:char
  * Return:int
@@ -43,30 +17,6 @@ int _strlen(char *s)
 return (i);
 }
 
-/**
- * _strcat - Concat Two String
- * @dest:First String
- * @src:Second String
- * Return:First String + Second String Char *
- */
-char *_strcat(char *dest, char *src)
-{
-	char *s = dest;
-
-	while (*dest != '\0')
-	{
-		dest++;
-	}
-
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (s);
-}
 /**
  * _strchr - Locate Charactere In String
  * @s:String Search In
